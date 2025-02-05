90f21ecd9254f8da7f8cfe4e518475fb220b3b22
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-02-06 00:10:09 +0600

    style: add className for improved styling in SimpleForm component

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -57,6 +57,7 @@ const SimpleForm = () => {
             type="text"
             id="name"
             name="name"
+            
             className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
             placeholder="Enter your name"
           />

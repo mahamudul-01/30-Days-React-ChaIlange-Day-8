38409d6b7fc2eb547f8c7be6b32699a3d8007fca
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-02-06 23:44:16 +0600

    add TresureContext to Grandpa component for context management

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,16 +1,21 @@
+import { createContext } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
+
+const TresureContext=createContext();
 
 const Grandpa = () => {
     return (
         <div className="w-5xl border-2 border-blue-400 p-4 text-center text-5xl mx-auto mt-6 rounded-xl font-bold" >
             <h2>Grandpa</h2>
-            <div className="flex justify-between border-2 border-blue-400 p-4 rounded-xl font-bold mt-4">
+           <TresureContext.Provider value='Diamond'>
+           <div className="flex justify-between border-2 border-blue-400 p-4 rounded-xl font-bold mt-4">
             <Dad></Dad>
             <Uncle></Uncle>
             <Aunty></Aunty>
             </div>
+            </TresureContext.Provider>
         </div>
     );
 };

c49220d24501e2b588ffbbf12e68c490f78f924a
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-02-07 17:40:16 +0600

    add GranpaMoney context and button to give money in Dad component

--- src/components/Dad/Dad.jsx
+++ src/components/Dad/Dad.jsx
@@ -1,19 +1,27 @@
+import { useContext } from "react";
 import Brother from "./Brother/Brother";
 import MySelf from "./Myself/MySelf";
 import Sister from "./Sister/Sister";
+import { GranpaMoney } from "../Grandpa/Grandpa";
 
 const Dad = () => {
-    return (
-        <div className="text-3xl border-2 border-blue-400 p-2  rounded-xl font-bold">
-            <h2>Dad</h2>
-            <div className="flex justify-between border-2 border-blue-400 p-4 rounded-xl font-bold mt-4 space-x-4">
-                <MySelf></MySelf>
-                <Brother></Brother>
-                <Sister></Sister>
-
-            </div>
-        </div>
-    );
+  const [money,setMoney]  = useContext(GranpaMoney);
+  return (
+    <div className="text-3xl border-2 border-blue-400 p-2 rounded-xl font-bold">
+      <h2>Dad</h2>
+      <div className="flex justify-between border-2 border-blue-400 p-4 rounded-xl font-bold mt-4 space-x-4">
+        <MySelf></MySelf>
+        <Brother></Brother>
+        <Sister></Sister>
+      </div>
+      <button
+        className="bg-blue-400 text-white px-4 py-2 mt-4 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-600"
+        onClick={() => setMoney(money + 100)}
+      >
+        Give Your Father Money 100
+      </button>
+    </div>
+  );
 };
 
 export default Dad;
--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,22 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 
 export const TresureContext=createContext();
+export const GranpaMoney=createContext();
+
+
 
 const Grandpa = () => {
+    const gift='Diamond';
+    const [money,setMoney]=useState(0);
     return (
         <div className="w-5xl border-2 border-blue-400 p-4 text-center text-5xl mx-auto mt-6 rounded-xl font-bold" >
             <h2>Grandpa</h2>
             <p>Grandpa has some diamond . how he gift it</p>
-           <TresureContext.Provider value='Diamond'>
+            <p>Balance : {money}</p>
+           <GranpaMoney.Provider value={[money,setMoney]} >
+           <TresureContext.Provider value={gift}>
            <div className="flex justify-between border-2 border-blue-400 p-4 rounded-xl font-bold mt-4">
             <Dad></Dad>
             <Uncle></Uncle>
             <Aunty></Aunty>
             </div>
             </TresureContext.Provider>
+              </GranpaMoney.Provider>
         </div>
     );
 };
